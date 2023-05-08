77bffc2a9d54bb799d3c28ad387f454307019802
MayuriRajkumarYerme <68225107+MayuriRajkumarYerme@users.noreply.github.com>
2023-05-08 10:12:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
-var x=10;
-console.log(x);
+const express = require("express");
+const app = express();
 
+//Requst processing
+
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to Hospital Management System</h1>"+
+                   "<hr/>"+
+                   "<ol>"+
+                    "<li> PGDAC</li>"+
+                    "<li> PGDBDA</li>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>Transflower Learning Pvt. ltd.</h1>");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<p>Contact Number:9881735801</p>"
+            +"<p>Email:ravi.tambade</p>" );
+});
+
+app.get("/flowers",(req, res)=>{
+    var products=[
+    {"id":12,"title":"Gerbera","description":"Wedding flower","unitprice":5,"quantity":6000},
+    {"id":13,"title":"Rose","description":"Valntine flower","unitprice":15,"quantity":4000},
+    {"id":14,"title":"Lotus","description":"Worship flower","unitprice":25,"quantity":16000},
+    {"id":15,"title":"Jasmine","description":"Smelling  flower","unitprice":2,"quantity":6650},
+ 
+    ];
+    res.send(products);
+
+})
+app.listen(3000, () => {
+    console.log("Listen on the port 3000...");
+});
